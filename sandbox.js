26044c563459537dde3b290e02a635da1366de2c
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-04 12:22:21 -0700

    fizzbuzz

--- sandbox.js
+++ sandbox.js
@@ -18,3 +18,21 @@ function oddishOrEvenish(input) {
     return "Oddish";
   }
 }
+
+//FIZZ BUZZ
+function fizzBuzz(n) {
+  const results = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0) {
+      results.push("Fizz");
+    } else if (i % 5 == 0) {
+      results.push("Buzz");
+    } else if (i % 15 == 0) {
+      results.push("FizzBuzz");
+    } else {
+      results.push(i);
+    }
+  }
+  return results;
+}
