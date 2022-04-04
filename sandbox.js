1cd2ee9effa960f223bbe6658cc49512f8232041
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-04 10:06:14 -0700

    reverseWords challenge complete

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+// REVERSE WORDS
+function reverseWords(input) {
+  const result = input.split("").reverse().join("");
+  return result;
+}
+
+console.log(reverseWords("What does this do"));
+// TITLE CASE WORDS
